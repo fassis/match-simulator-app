052e8f9110dacc98e0acdaa48fc3cb95e6e9dc68
_assis <zyx.assis@gmail.com>
2022-04-09 23:57:05 -0300

    Domain implementation

---
+++ app/src/main/java/com/example/simulator/ui/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.simulator.ui;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.simulator.R;
+import com.example.simulator.data.MatchesApi;
+import com.example.simulator.databinding.ActivityMainBinding;
+import com.example.simulator.domain.Match;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private MatchesApi matchesApi;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setupHttpClient();
+        setupMatchesList();
+        setupMatchesRefresh();
+        setupFloatingActionButton();
+
+    }
+
+    private void setupHttpClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://github.com/fassis/matches-simulator-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        matchesApi = retrofit.create(MatchesApi.class);
+    }
+
+    private void setupFloatingActionButton() {
+        //TODO
+    }
+
+    private void setupMatchesRefresh() {
+        //TODO
+    }
+
+    private void setupMatchesList() {
+        matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
+            @Override
+            public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+                if (response.isSuccessful()){
+                    List<Match> matches = response.body();
+                    Log.i("SIMULATOR","Deu certo! Partidas = "+matches.size());
+                } else {
+                    showErrorMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Match>> call, Throwable t) {
+                showErrorMessage();
+            }
+        });
+    }
+
+    private void showErrorMessage() {
+        Snackbar.make(binding.fabSimulate, R.string.error_api, Snackbar.LENGTH_SHORT).show();
+    }
+}
